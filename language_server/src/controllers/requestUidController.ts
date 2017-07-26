bcec8552ed63e10bfdfa4168a8f22b49e2e15e6b
t-chaohe <t-chaohe@microsoft.com>
2017-07-26 22:01:40 +0800

    two project

---
+++ language_server/src/controllers/requestUidController.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode';
+import {httpRequestFactory} from '../utility/httpRequestFactory';
+
+export class requestUidController {
+
+    static sync getCompletionItem(partUid: string): void{
+        return ;
+    }
+}
